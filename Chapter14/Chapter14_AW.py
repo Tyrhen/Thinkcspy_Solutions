c6071850bc2fb3a7d13e722fec4ef356b280adb7
Tyrhen <66821494+Tyrhen@users.noreply.github.com>
2020-11-22 00:23:31 -0500

    Chapter 14 exercises
    cleaner formatting using Black

--- Chapter14/Chapter14_AW.py
+++ Chapter14/Chapter14_AW.py
@@ -9,34 +9,36 @@ must read a file and return its content in a list
 from unit_tester import test
 import time
 
-def linear_search(array,target):
+
+def linear_search(array, target):
     """Searchs for an item in a list in a linear fashion"""
-    for i,item in enumerate(array):
+    for i, item in enumerate(array):
         if item == target:
             return i
     return -1
 
-def binary_search(array,target):
+
+def binary_search(array, target):
     """implementation of binary search to find a target item"""
     low = 0
     high = len(array)
 
     while True:
 
-       if low == high:
-           return -1
-        
-       mid_index = (high+low)//2
-       mid_item = array[mid_index]
+        if low == high:
+            return -1
 
-       if mid_item == target:
-           return mid_index
-        
-       elif mid_item < target:
-           low = mid_index+1
-           
-       else:
-           high = mid_index
+        mid_index = (high + low) // 2
+        mid_item = array[mid_index]
+
+        if mid_item == target:
+            return mid_index
+
+        elif mid_item < target:
+            low = mid_index + 1
+
+        else:
+            high = mid_index
 
 
 def get_words_in_file(filename):
@@ -47,18 +49,18 @@ def get_words_in_file(filename):
     words = clean_text(content)
     return words
 
+
 def clean_text(content):
     """takes a string and returns the punctuation-free lowercase string version in a list"""
     my_substitutions = content.maketrans(
-      
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&()*+,-./:;<=>?@[]^_`{|}~'\\",
-      
-      "abcdefghijklmnopqrstuvwxyz                                          ")
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&()*+,-./:;<=>?@[]^_`{|}~'\\",
+        "abcdefghijklmnopqrstuvwxyz                                          ",
+    )
 
-    
     cleaned_text = content.translate(my_substitutions)
     wds = cleaned_text.split()
     return wds
+
 
 def remove_adjacent_dups(array):
     """ removes adjacent duplicates from a list"""
@@ -71,12 +73,13 @@ def remove_adjacent_dups(array):
 
     return no_duplicates
 
-def merge(array1,array2):
+
+def merge(array1, array2):
     """ merges two sorted list into a single sorted list"""
     array1_pointer = 0
     array2_pointer = 0
     merged = []
-    
+
     while True:
         if array1_pointer >= len(array1):
             merged.extend(array2[array2_pointer:])
@@ -88,20 +91,18 @@ def merge(array1,array2):
             print(merged)
 
             return merged
-        
-    
+
         if array1[array1_pointer] <= array2[array2_pointer]:
             merged.append(array1[array1_pointer])
             array1_pointer += 1
-            
+
         else:
             array1[array1_pointer] > array2[array2_pointer]
             merged.append(array2[array2_pointer])
             array2_pointer += 1
-          
-            
-    
-def find_unknown_merge_pattern(vocab,wds):
+
+
+def find_unknown_merge_pattern(vocab, wds):
     """Both the vocab and the words must be sorted. Returns a new list of words from wds that do not occur in vocab"""
     xi = 0
     yi = 0
@@ -121,79 +122,75 @@ def find_unknown_merge_pattern(vocab,wds):
         else:
             result.append(wds[yi])
             yi += 1
-    
-def unknown_words(vocab,text):
+
+
+def unknown_words(vocab, text):
     """collects all the words that appear in text that are not in the vocab"""
     unknown = []
     for word in text:
-        if binary_search(vocab,word) < 0:
+        if binary_search(vocab, word) < 0:
             unknown.append(word)
     return unknown
-            
-        
-    
+
+
 def test_suite():
     friends = "Joe Zoe Brad Angelina Zuki Thandi Paris".split()
-    print('\n Linear Search')
-    test(linear_search(friends,'Zoe')==1)
-    test(linear_search(friends,'Joe')==0)
-    test(linear_search(friends,'Paris')==6)
-    test(linear_search(friends,'Bill')==-1)
+    print("\n Linear Search")
+    test(linear_search(friends, "Zoe") == 1)
+    test(linear_search(friends, "Joe") == 0)
+    test(linear_search(friends, "Paris") == 6)
+    test(linear_search(friends, "Bill") == -1)
 
-    print('\n Unknown Words')
-    vocab = 'apple boy dog down fell girl grass the tree'.split()
-    book_words = 'the apple fell from the tree to the grass'.split()
-    test(unknown_words(vocab, book_words) == ['from','to'])
+    print("\n Unknown Words")
+    vocab = "apple boy dog down fell girl grass the tree".split()
+    book_words = "the apple fell from the tree to the grass".split()
+    test(unknown_words(vocab, book_words) == ["from", "to"])
     test(unknown_words([], book_words) == book_words)
-    test(unknown_words(vocab, ['the','boy','fell']) == [])
+    test(unknown_words(vocab, ["the", "boy", "fell"]) == [])
 
-    print('\n clean_text')
-    test(clean_text('My name is Earl!') == 'my name is earl'.split())
-    test(clean_text('Well, I never!, said Alice.')==
-         'well i never said alice'.split())
+    print("\n clean_text")
+    test(clean_text("My name is Earl!") == "my name is earl".split())
+    test(clean_text("Well, I never!, said Alice.") == "well i never said alice".split())
 
-    print('\n Binary Search')
-    xs = [2,3,5,7,11,13,17,23,29,31,37,43,47,53]
-    test(binary_search(xs,20)==-1)
-    test(binary_search(xs,99)==-1)
-    test(binary_search(xs,1)==-1)
-    for (i,v) in enumerate(xs):
-        test(binary_search(xs,v) == i)
+    print("\n Binary Search")
+    xs = [2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37, 43, 47, 53]
+    test(binary_search(xs, 20) == -1)
+    test(binary_search(xs, 99) == -1)
+    test(binary_search(xs, 1) == -1)
+    for (i, v) in enumerate(xs):
+        test(binary_search(xs, v) == i)
 
-    print('\n Remove Duplicates')
-    test(remove_adjacent_dups([1,2,3,3,3,3,5,6,9,9])==[1,2,3,5,6,9])
-    test(remove_adjacent_dups([])==[])
-    test(remove_adjacent_dups(['a','big','big','bite','dog'])==['a','big','bite','dog'])
+    print("\n Remove Duplicates")
+    test(remove_adjacent_dups([1, 2, 3, 3, 3, 3, 5, 6, 9, 9]) == [1, 2, 3, 5, 6, 9])
+    test(remove_adjacent_dups([]) == [])
+    test(
+        remove_adjacent_dups(["a", "big", "big", "bite", "dog"])
+        == ["a", "big", "bite", "dog"]
+    )
 
-    print('\n Merge Sorted')
-    xs = [1,3,5,7,9,11,13,15,17,19]
-    ys = [4,8,12,16,20,24]
-    zs = xs+ys
+    print("\n Merge Sorted")
+    xs = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    ys = [4, 8, 12, 16, 20, 24]
+    zs = xs + ys
     zs.sort()
-    test(merge(xs,[]) == [])
-    test(merge(xs,ys) == zs)
-    test(merge([1,2,3],[3,4,5]) == [1,2,3,3,4,5])
-    test(merge(['a','big','cat'],['big','bite','dog']) == ['a','big','big','bite','cat','dog'])
-
- 
-    
-
-
+    test(merge(xs, []) == [])
+    test(merge(xs, ys) == zs)
+    test(merge([1, 2, 3], [3, 4, 5]) == [1, 2, 3, 3, 4, 5])
+    test(
+        merge(["a", "big", "cat"], ["big", "bite", "dog"])
+        == ["a", "big", "big", "bite", "cat", "dog"]
+    )
 
 
-bigger_vocab = get_words_in_file('vocab.txt')
-all_words = get_words_in_file('alice_in_wonderland.txt')
+bigger_vocab = get_words_in_file("vocab.txt")
+all_words = get_words_in_file("alice_in_wonderland.txt")
 
 
 t0 = time.process_time()
 all_words.sort()
 book_words = remove_adjacent_dups(all_words)
-missing_words = find_unknown_merge_pattern(bigger_vocab,book_words)
+missing_words = find_unknown_merge_pattern(bigger_vocab, book_words)
 t1 = time.process_time()
 
-print('There are {0} unknown words.'.format(len(missing_words)))
-print('that took {0:.4f} seconds.'.format(t1-t0))
-
-
-
-    
+print("There are {0} unknown words.".format(len(missing_words)))
+print("that took {0:.4f} seconds.".format(t1 - t0))
--- Chapter14/Chapter14_EightQueens.py
+++ Chapter14/Chapter14_EightQueens.py
@@ -9,34 +9,37 @@ without being able to attack one another
 from unit_tester import test
 
 
-def share_diagonal(x0,y0,x1,y1):
+def share_diagonal(x0, y0, x1, y1):
     """Compute if two coordinates are diagonal to each other"""
     dx = abs(x0 - x1)
     dy = abs(y0 - y1)
     return dx == dy
-   
 
-def col_clashes(bs,c):
+
+def col_clashes(bs, c):
     for i in range(c):
-        if share_diagonal(i,bs[i], c, bs[c]):
+        if share_diagonal(i, bs[i], c, bs[c]):
             return True
     return False
+
 
 def has_clashes(chess_board):
-    """ Determine whether we have any queens clashing on the diagonals.
-        We're assuming here that the_board is a permutation of column
-        numbers, so we're not explicitly checking row or column clashes.
+    """Determine whether we have any queens clashing on the diagonals.
+    We're assuming here that the_board is a permutation of column
+    numbers, so we're not explicitly checking row or column clashes.
     """
-    for col in range(1,len(chess_board)):
-        if col_clashes(chess_board,col):
+    for col in range(1, len(chess_board)):
+        if col_clashes(chess_board, col):
             return True
     return False
-    
+
+
 def main(x):
-    """ returns unique solutions to the Queen Placement Problem
-        given size of the board"""
+    """returns unique solutions to the Queen Placement Problem
+    given size of the board"""
     import random
     import time
+
     rng = random.Random()
     bd = list(range(x))
     num_found = 0
@@ -47,46 +50,38 @@ def main(x):
     while num_found < 50:
         rng.shuffle(bd)
         tries += 1
-        
-         
+
         if not has_clashes(bd) and bd not in found_solutions:
-                print('Found unique solution {0} in {1} tries'.format(bd, tries))
-                num_found += 1
-                found_solutions.append(list(bd))
-    
-                
-                tries = 0
-                
+            print("Found unique solution {0} in {1} tries".format(bd, tries))
+            num_found += 1
+            found_solutions.append(list(bd))
+
+            tries = 0
+
     t1 = time.process_time()
-    print('{0} solutions found in {1} seconds'.format(num_found,t1-t0))
-    
-    
-            
-        
+    print("{0} solutions found in {1} seconds".format(num_found, t1 - t0))
+
+
 def test_suite():
-    print('\n share diagonals')
-    test(not share_diagonal(5,2,2,0))
-    test(share_diagonal(5,2,3,0))
-    test(share_diagonal(5,2,4,3))
-    test(share_diagonal(5,2,4,1))
+    print("\n share diagonals")
+    test(not share_diagonal(5, 2, 2, 0))
+    test(share_diagonal(5, 2, 3, 0))
+    test(share_diagonal(5, 2, 4, 3))
+    test(share_diagonal(5, 2, 4, 1))
 
-    print('\n Column Clashes')
-    test(not col_clashes([6,4,2,0,5], 4))
-    test(not col_clashes([6,4,2,0,5,7,1,3], 7))
-    test(col_clashes([0,1], 1))
-    test(col_clashes([5,6], 1))
-    test(col_clashes([6,5], 1))
-    test(col_clashes([0,6,4,3], 3))
-    test(col_clashes([5,0,7], 2))
-    test(not col_clashes([2,0,1,3], 1))
-    test(col_clashes([2,0,1,3], 2))
+    print("\n Column Clashes")
+    test(not col_clashes([6, 4, 2, 0, 5], 4))
+    test(not col_clashes([6, 4, 2, 0, 5, 7, 1, 3], 7))
+    test(col_clashes([0, 1], 1))
+    test(col_clashes([5, 6], 1))
+    test(col_clashes([6, 5], 1))
+    test(col_clashes([0, 6, 4, 3], 3))
+    test(col_clashes([5, 0, 7], 2))
+    test(not col_clashes([2, 0, 1, 3], 1))
+    test(col_clashes([2, 0, 1, 3], 2))
 
-    print('\n Column Clashes')
-    test(not has_clashes([6,4,2,0,5,7,1,3])) 
-    test(has_clashes([4,6,2,0,5,7,1,3]))     
-    test(has_clashes([0,1,2,3]))             
-    test(not has_clashes([2,0,3,1])) 
-
-
-    
-
+    print("\n Column Clashes")
+    test(not has_clashes([6, 4, 2, 0, 5, 7, 1, 3]))
+    test(has_clashes([4, 6, 2, 0, 5, 7, 1, 3]))
+    test(has_clashes([0, 1, 2, 3]))
+    test(not has_clashes([2, 0, 3, 1]))
